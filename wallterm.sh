8a3fb06f24b960afe55a8ecb9868d1c369584d67
Taekus <taek@taek.us>
2019-10-20 01:47:42 -0700

    Preping for Konsole commands

--- wallterm.sh
+++ wallterm.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
     # Author Taekus
     # taek.us
+    # Depends wmctrl
 
 title=bgcon # Window Title for the Terminal Window
-term=xfce4 # Which Terminal Emulator You Are Using, gnome or xfce4 for now
-hw=80x30 #  Height and width based on Cols/Rows of the terminal window (ie 80Colsx30Rows)
+cols=80 # Width in Columns
+rows=30 # Height in Rows
 loc=+0+0 # Location on screen in pixels
-exec="/usr/bin/gotop" # Which program to run in the terminal window on launch
+term=xfce4-terminal # Which Terminal Emulator You Are Using, gnome or xfce4 for now
+opts="--hide-menubar --hide-borders --hide-toolbar --hide-scrollbar"
+exec="/usr/bin/gotop -b" # Which program to run in the terminal window on launch
 
-${term}-terminal --hide-menubar --hide-borders --hide-toolbar --hide-scrollbar --title=${title} --geometry=${hw}${loc} --command=${exec} &
-wmctrl -r ${title} -b add,below,sticky
+${term} ${opts} --title=${title} --geometry=${hw}${loc} --command="${exec}" &
+sleep 1
+wmctrl -r ${title} -b add,below,sticky # Split wmctrl calls due to ARGs limitations
 wmctrl -r ${title} -b add,skip_taskbar,skip_pager

2ccfbb335b2e6b3c1efd9c33da5f7bd415f6e406
Taekus <taek@taek.us>
2019-10-21 00:13:41 -0700

    fixed issues for xfce4 and similar terminals

--- wallterm.sh
+++ wallterm.sh
@@ -9,12 +9,12 @@ rows=30 # Height in Rows
 loc=+0+0 # Location on screen in pixels
 term=xfce4-terminal # Which Terminal Emulator You Are Using, gnome-terminal or xfce4-terminal for now (testing konsole)
 dir="/home/$USER/" # WorkDirectory, defaulting to user home dir
-xopts="--hide-menubar --no-borders --hide-toolbar --hide-scrollbar" # xterm,xfce4,gnome terminal options
+xopts="--hide-menubar --hide-borders --hide-toolbar --hide-scrollbar" # xterm,xfce4,gnome terminal options
 kopts="--hide-menubar --noframe --hide-tabbar --hide-scrollbar" # xterm,xfce4,gnome terminal options
 exec="/usr/bin/gotop -b" # Which program to run in the terminal window on launch
 
 if [[ (${term} = "xfce4-terminal") || (${term} = "gnome-terminal") || (${term} = "xterm") ]]; then
-    ${term} ${xopts} --title=${title} --geometry=${cols}x${rows}${loc} --command=`${exec}` &
+    ${term} ${xopts} --title=${title} --geometry=${cols}x${rows}${loc} --command="${exec}" &
 elif [[ ${term} = "konsole" ]]; then
     ${term} --workdir ${dir} ${kopts} --separate -p `TerminalColumns=${cols}` -p `TerminalRows=${rows}` -geometry ${loc} -e `qdbus org.kde.konsole $KONSOLE_DBUS_SESSION setTitle 1 ${title}; ${exec};`
 fi

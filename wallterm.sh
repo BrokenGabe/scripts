0c013eb541e760f9b22f02fe5592dbcb4493cdee
Taekus <taek@taek.us>
2019-10-20 23:16:53 -0700

    I need to get the hang of this markdown

--- wallterm.sh
+++ wallterm.sh
@@ -8,13 +8,14 @@ cols=80 # Width in Columns
 rows=30 # Height in Rows
 loc=+0+0 # Location on screen in pixels
 term=xfce4-terminal # Which Terminal Emulator You Are Using, gnome-terminal or xfce4-terminal for now (testing konsole)
-opts="--hide-menubar --hide-borders --hide-toolbar --hide-scrollbar" # xterm,xfce4,gnome terminal options
+dir="/home/$USER/" # WorkDirectory, defaulting to user home dir
+opts="--hide-menubar --hide-toolbar --hide-scrollbar" # xterm,xfce4,gnome terminal options
 exec="/usr/bin/gotop -b" # Which program to run in the terminal window on launch
 
 if [[ (${term} = "xfce4-terminal") || (${term} = "gnome-terminal") || (${term} = "xterm") ]]; then
-    ${term} ${opts} --title=${title} --geometry=${cols}x${rows}${loc} --command="${exec}" &
+    ${term} ${opts} --hide-borders --title=${title} --geometry=${cols}x${rows}${loc} --command=`${exec}` &
 elif [[ ${term} = "konsole" ]]; then
-    ${term} --workdir /home/$USER/ ${opts} --separate -p `TerminalColumns=${cols}` -p `TerminalRows=${rows}` -geometry ${loc} -e `qdbus org.kde.konsole $KONSOLE_DBUS_SESSION setTitle 1 ${title}; ${exec};`
+    ${term} --workdir ${dir} ${opts} --noframe --separate -p `TerminalColumns=${cols}` -p `TerminalRows=${rows}` -geometry ${loc} -e `qdbus org.kde.konsole $KONSOLE_DBUS_SESSION setTitle 1 ${title}; ${exec};`
 fi
     #I hope the above works, without konsole, I cannot test this.
 sleep 1
